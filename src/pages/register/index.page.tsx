6bd3a2047fa44a538dd30d7e4e4d2c46b057cd2d
LucasDev <LucasDev9645@gmail.com>
2024-01-21 09:58:37 -0300

    Estrutura visual do cadastro

---
+++ src/pages/register/index.page.tsx
@@ -0,0 +1,35 @@
+import { ArrowRight } from "phosphor-react";
+
+import { Button, Heading, MultiStep, Text, TextInput } from "@ignite-ui/react";
+
+import { Container, Form, Header } from "./style";
+
+const Register = () => {
+  return (
+    <Container>
+      <Header>
+        <Heading as="strong">Bem-vindo ao Ignite Call!</Heading>
+        <Text>
+          Precisamos de algumas informações para criar seu perfil! Ah, você pode
+          editar essas informações depois.
+        </Text>
+        <MultiStep size={4} currentStep={1} />
+      </Header>
+      <Form as="form">
+        <label>
+          <Text size="sm">Nome de usuário</Text>
+          <TextInput prefix="ignite.com/" placeholder="Seu-usuario" />
+        </label>
+        <label>
+          <Text size="sm">Nome completo</Text>
+          <TextInput placeholder="Seu nome" />
+        </label>
+        <Button typr="submit">
+          Próximo Passo <ArrowRight />
+        </Button>
+      </Form>
+    </Container>
+  );
+};
+
+export default Register;
